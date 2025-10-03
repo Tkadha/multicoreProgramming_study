1286039e302a24d927ac154ad3ceb4d50dbcc06f
Tkadha <84236232+Tkadha@users.noreply.github.com>
2025-10-03 14:37:14 +0900

    feat: add lazy synchronization set

--- NB_algorithm_SET.cpp
+++ NB_algorithm_SET.cpp
@@ -6,15 +6,16 @@
 #include <numeric>
 
 using namespace std::chrono;
-const int MAX_THREADS = 32;
+const int MAX_THREADS = 16;
 
 class NODE {
 public:
 	int value;
 	NODE* next;
 	std::mutex mu;
-	NODE() :next(nullptr) {}
-	NODE(int x) :next(nullptr), value(x) {}
+	bool removed;
+	NODE() :next(nullptr), removed(false) {}
+	NODE(int x) :next(nullptr), value(x), removed(false) {}
 	void lock() { mu.lock(); }
 	void unlock() { mu.unlock(); }
 };
@@ -345,14 +346,204 @@ public:
 	}
 };
 
+class L_SET {
+private:
+	NODE* head, * tail;
+public:
+	L_SET() {
+		head = new NODE(std::numeric_limits<int>::min());
+		tail = new NODE(std::numeric_limits<int>::max());
+		head->next = tail;
+	}
+	~L_SET() {
+		clear();
+		delete head;
+		delete tail;
+	}
+	void clear() {
+		NODE* curr = head->next;
+		while (curr != tail) {
+			NODE* temp = curr;
+			curr = curr->next;
+			delete temp;
+		}
+		head->next = tail;
+	}
 
-O_SET set;
-void benchmark(const int num_thread)
+	bool validate(int x, NODE* p, NODE* c) {
+		return (!p->removed && !c->removed && p->next == c);
+	}
+
+	bool add(int x) {
+		while (true) {
+			NODE* prev = head;
+			NODE* curr = prev->next;
+
+			while (curr->value < x) {
+				prev = curr;
+				curr = curr->next;
+			}
+			prev->lock(); curr->lock();
+			if (false == validate(x, prev, curr)) {
+				prev->unlock(); curr->unlock();
+				continue;
+			}
+			if (curr->value == x) {
+				prev->unlock(); curr->unlock();
+				return false;
+			}
+			else {
+				NODE* newnode = new NODE(x);
+				newnode->next = curr;
+				prev->next = newnode;
+				prev->unlock(); curr->unlock();
+				return true;
+			}
+		}
+
+	}
+	bool remove(int x) {
+		while (true) {
+			NODE* prev = head;
+			NODE* curr = prev->next;
+
+			while (curr->value < x) {
+				prev = curr;
+				curr = curr->next;
+			}
+
+			prev->lock(); curr->lock();
+			if (false == validate(x, prev, curr)) {
+				prev->unlock(); curr->unlock();
+				continue;
+			}
+			if (curr->value == x) {
+
+				curr->removed = true;
+				prev->next = curr->next;
+				prev->unlock(); curr->unlock();
+				//delete temp;
+				return true;
+			}
+			else {
+				prev->unlock(); curr->unlock();
+				return false;
+			}
+		}
+	}
+	bool contains(int x) {
+		while (true) {
+			NODE* prev = head;
+			while (prev->value < x) {
+				prev = prev->next;
+			}
+			return prev->value == x && !prev->removed;
+		}
+	}
+	void print20() {
+		auto curr = head->next;
+		for (int i = 0;i < 20 && curr != tail;++i) {
+			if (!curr->removed) {
+				std::cout << curr->value << ", ";
+			}
+			else --i;
+			curr = curr->next;
+		}
+		std::cout << std::endl;
+	}
+};
+
+
+L_SET set;
+
+
+const int LOOP = 400'0000;
+const int RANGE = 1000;
+
+
+#include <array>
+
+class HISTORY {
+public:
+	int op;
+	int i_value;
+	bool o_value;
+	HISTORY(int o, int i, bool re) : op(o), i_value(i), o_value(re) {}
+};
+
+std::array<std::vector<HISTORY>, MAX_THREADS> history;
+
+void check_history(int num_threads)
 {
-	const int loop_count = 4'000'000 / num_thread;
-	const int range = 1000;
-	for (int i = 0;i < loop_count;++i) {
-		int value = rand() % range;
+	std::array <int, RANGE> survive = {};
+	std::cout << "Checking Consistency : ";
+	if (history[0].size() == 0) {
+		std::cout << "No history.\n";
+		return;
+	}
+	for (int i = 0; i < num_threads; ++i) {
+		for (auto& op : history[i]) {
+			if (false == op.o_value) continue;
+			if (op.op == 3) continue;
+			if (op.op == 0) survive[op.i_value]++;
+			if (op.op == 1) survive[op.i_value]--;
+		}
+	}
+	for (int i = 0; i < RANGE; ++i) {
+		int val = survive[i];
+		if (val < 0) {
+			std::cout << "ERROR. The value " << i << " removed while it is not in the set.\n";
+			exit(-1);
+		}
+		else if (val > 1) {
+			std::cout << "ERROR. The value " << i << " is added while the set already have it.\n";
+			exit(-1);
+		}
+		else if (val == 0) {
+			if (set.contains(i)) {
+				std::cout << "ERROR. The value " << i << " should not exists.\n";
+				exit(-1);
+			}
+		}
+		else if (val == 1) {
+			if (false == set.contains(i)) {
+				std::cout << "ERROR. The value " << i << " shoud exists.\n";
+				exit(-1);
+			}
+		}
+	}
+	std::cout << " OK\n";
+}
+
+void benchmark_check(int num_threads, int th_id)
+{
+	for (int i = 0; i < LOOP / num_threads; ++i) {
+		int op = rand() % 3;
+		switch (op) {
+		case 0: {
+			int v = rand() % RANGE;
+			history[th_id].emplace_back(0, v, set.add(v));
+			break;
+		}
+		case 1: {
+			int v = rand() % RANGE;
+			history[th_id].emplace_back(1, v, set.remove(v));
+			break;
+		}
+		case 2: {
+			int v = rand() % RANGE;
+			history[th_id].emplace_back(2, v, set.contains(v));
+			break;
+		}
+		}
+	}
+}
+
+
+void benchmark(const int num_threads)
+{
+	for (int i = 0; i < LOOP / num_threads; ++i) {
+		int value = rand() % RANGE;
 		int op = rand() % 3;
 		if (op == 0) set.add(value);
 		else if (op == 1) set.remove(value);
@@ -362,17 +553,40 @@ void benchmark(const int num_thread)
 
 int main()
 {
-	for (int i = 1; i <= MAX_THREADS; i *= 2) {
+	using namespace std::chrono;
+	for (int num_thread = MAX_THREADS; num_thread >= 1; num_thread /= 2) {
 		set.clear();
 		std::vector<std::thread> threads;
-		auto start_t = high_resolution_clock::now();
-		for (int j = 0; j < i; ++j) {
-			threads.emplace_back(benchmark, i);
-		}
-		for (auto& th : threads) th.join();
-		auto end_t = high_resolution_clock::now();
-		std::cout << "Thread " << i << ", time: " << duration_cast<milliseconds>(end_t - start_t).count() << " ms" << std::endl;
-		std::cout << "Set: ";
-		set.print20();
+		auto start = high_resolution_clock::now();
+		for (int i = 0; i < num_thread; ++i)
+			threads.emplace_back(benchmark, num_thread);
+		for (auto& th : threads)
+			th.join();
+		auto stop = high_resolution_clock::now();
+		auto duration = duration_cast<milliseconds>(stop - start);
+		std::cout << "Threads: " << num_thread
+			<< ", Duration: " << duration.count() << " ms.\n";
+		std::cout << "Set: "; set.print20();
+	}
+
+	// Consistency check
+	std::cout << "\n\nConsistency Check\n";
+
+	for (int num_thread = MAX_THREADS; num_thread >= 1; num_thread /= 2) {
+		set.clear();
+		std::vector<std::thread> threads;
+		for (int i = 0; i < MAX_THREADS; ++i)
+			history[i].clear();
+		auto start = high_resolution_clock::now();
+		for (int i = 0; i < num_thread; ++i)
+			threads.emplace_back(benchmark_check, num_thread, i);
+		for (auto& th : threads)
+			th.join();
+		auto stop = high_resolution_clock::now();
+		auto duration = duration_cast<milliseconds>(stop - start);
+		std::cout << "Threads: " << num_thread
+			<< ", Duration: " << duration.count() << " ms.\n";
+		std::cout << "Set: "; set.print20();
+		check_history(num_thread);
 	}
 }
